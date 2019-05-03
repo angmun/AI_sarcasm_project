3ed74e7eb1bec38a760247e91ed8fdd6077576e9
angmun <munyaoangelica@yahoo.com>
2019-05-03 14:57:56 -0400

    MLP model using n-gram vectors nearly complete: set up various network model configurations to test hyper-parameter effect on accuracy, with 10 epochs for all configurations.

--- sarcasm_classifier.py
+++ sarcasm_classifier.py
@@ -42,6 +42,9 @@
 # Deciding on number of hidden layers and nodes for a neural network:
 # https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
 
+# Input dimension mismatch issue:
+# https://github.com/keras-team/keras/issues/6351
+
 # Classifier and data processing packages
 from tensorflow.python.keras.preprocessing import sequence
 from tensorflow.python.keras.preprocessing import text
@@ -60,7 +63,8 @@ from matplotlib import pyplot as plotr
 
 # Data file reading and processing packages
 import pandas
-import sklearn_pandas as skp
+
+# import sklearn_pandas as skp
 
 # First load the data-set so we have access to its contents.
 # It is a json file so we open the file and read its contents line by line.
@@ -116,6 +120,7 @@ train_ind, test_ind = next(splitter.split(sarcasm_feature_matrix, sarcasm_target
 
 # NB: Linear SVM has a better score than Multinomial NB using their respective default hyperparameter values.
 
+
 # Third classifier: Neural Network
 # N-gram vectors:
 # Remove rare words from the generated vocabulary to improve the learning process.
@@ -159,17 +164,116 @@ train_ind, other_ind = next(splitter1.split(best_sarcasm_features_numpy, sarcasm
 val_ind, accest_ind = next(splitter2.split(best_sarcasm_features_numpy[other_ind], sarcasm_target_numpy[other_ind]))
 
 # Build a multi-layer network.
-the_layers = [layers.Dropout(0.2, input_shape=best_sarcasm_features_numpy.shape),
-                layers.Dense(128, activation='sigmoid'),
-                layers.Dropout(0.2),
+# 64 input for Dense layers:
+# With no dropout layers:
+the_layers = [layers.Dense(64, activation='sigmoid', input_shape=best_sarcasm_features_numpy.shape),
+              layers.Dense(64, activation='sigmoid'),
+              layers.Dense(1, activation='sigmoid')]
+networkMLP = Sequential(layers=the_layers)
+
+# Configure the network in preparation for training.
+networkMLP.compile(optimizer='adam', metrics=['accuracy'], loss='binary_crossentropy')
+
+# Train the model using the data.
+# Results in a history object that contains training and validation loss and metrics values.
+training064 = networkMLP.fit(best_sarcasm_features_numpy[train_ind], sarcasm_target_numpy[train_ind],
+                             validation_data=(best_sarcasm_features_numpy[val_ind], sarcasm_target_numpy[val_ind]),
+                             epochs=10)
+
+# With one dropout layer:
+the_layers = [layers.Dropout(0.2, noise_shape=best_sarcasm_features_numpy.shape),
+              layers.Dense(64, activation='sigmoid'),
+              layers.Dense(64, activation='sigmoid'),
+              layers.Dense(1, activation='sigmoid')]
+networkMLP = Sequential(layers=the_layers)
+
+# Configure the network in preparation for training.
+networkMLP.compile(optimizer='adam', metrics=['accuracy'], loss='binary_crossentropy')
+
+# Train the model using the data.
+# Results in a history object that contains training and validation loss and metrics values.
+training164 = networkMLP.fit(best_sarcasm_features_numpy[train_ind], sarcasm_target_numpy[train_ind],
+                             validation_data=(best_sarcasm_features_numpy[val_ind], sarcasm_target_numpy[val_ind]),
+                             epochs=10)
+
+# With two dropout layers:
+the_layers = [layers.Dropout(0.2, noise_shape=best_sarcasm_features_numpy.shape),
+              layers.Dense(64, activation='sigmoid'),
+              layers.Dropout(0.2),
+              layers.Dense(64, activation='sigmoid'),
+              layers.Dense(1, activation='sigmoid')]
+networkMLP = Sequential(layers=the_layers)
+
+# Configure the network in preparation for training.
+networkMLP.compile(optimizer='adam', metrics=['accuracy'], loss='binary_crossentropy')
+
+# Train the model using the data.
+# Results in a history object that contains training and validation loss and metrics values.
+training264 = networkMLP.fit(best_sarcasm_features_numpy[train_ind], sarcasm_target_numpy[train_ind],
+                             validation_data=(best_sarcasm_features_numpy[val_ind], sarcasm_target_numpy[val_ind]),
+                             epochs=10)
+
+# 128 input for Dense layers:
+# With no dropout layers:
+the_layers = [layers.Dense(128, activation='sigmoid', input_shape=best_sarcasm_features_numpy.shape),
               layers.Dense(128, activation='sigmoid'),
               layers.Dense(1, activation='sigmoid')]
 networkMLP = Sequential(layers=the_layers)
 
-# Compile the network.
+# Configure the network in preparation for training.
 networkMLP.compile(optimizer='adam', metrics=['accuracy'], loss='binary_crossentropy')
 
 # Train the model using the data.
-training = networkMLP.fit(best_sarcasm_features_numpy[train_ind], sarcasm_target_numpy[train_ind],
+# Results in a history object that contains training and validation loss and metrics values.
+training0128 = networkMLP.fit(best_sarcasm_features_numpy[train_ind], sarcasm_target_numpy[train_ind],
+                              validation_data=(best_sarcasm_features_numpy[val_ind], sarcasm_target_numpy[val_ind]),
+                              epochs=10)
+
+# With one dropout layer:
+the_layers = [layers.Dropout(0.2, noise_shape=best_sarcasm_features_numpy.shape),
+              layers.Dense(128, activation='sigmoid'),
+              layers.Dense(128, activation='sigmoid'),
+              layers.Dense(1, activation='sigmoid')]
+networkMLP = Sequential(layers=the_layers)
+
+# Configure the network in preparation for training.
+networkMLP.compile(optimizer='adam', metrics=['accuracy'], loss='binary_crossentropy')
+
+# Train the model using the data.
+# Results in a history object that contains training and validation loss and metrics values.
+training1128 = networkMLP.fit(best_sarcasm_features_numpy[train_ind], sarcasm_target_numpy[train_ind],
+                              validation_data=(best_sarcasm_features_numpy[val_ind], sarcasm_target_numpy[val_ind]),
+                              epochs=10)
+
+# With two dropout layers:
+the_layers = [layers.Dropout(0.2, noise_shape=best_sarcasm_features_numpy.shape),
+              layers.Dense(128, activation='sigmoid'),
+              layers.Dropout(0.2),
+              layers.Dense(128, activation='sigmoid'),
+              layers.Dense(1, activation='sigmoid')]
+networkMLP = Sequential(layers=the_layers)
+
+# Configure the network in preparation for training.
+networkMLP.compile(optimizer='adam', metrics=['accuracy'], loss='binary_crossentropy')
+
+# Train the model using the data.
+# Results in a history object that contains training and validation loss and metrics values.
+training2128 = networkMLP.fit(best_sarcasm_features_numpy[train_ind], sarcasm_target_numpy[train_ind],
                           validation_data=(best_sarcasm_features_numpy[val_ind], sarcasm_target_numpy[val_ind]),
                           epochs=10)
+
+# Plot the learning curves for the six configurations of the network:
+plotr.plot(training064.epoch, training064.history['val_acc'], label='64 units, 0 dropout')
+plotr.plot(training164.epoch, training164.history['val_acc'], label='64 units, 1 dropout')
+plotr.plot(training264.epoch, training264.history['val_acc'], label='64 units, 2 dropouts')
+plotr.plot(training0128.epoch, training0128.history['val_acc'], label='128 units, 0 dropout')
+plotr.plot(training1128.epoch, training1128.history['val_acc'], label='128 units, 1 dropout')
+plotr.plot(training2128.epoch, training2128.history['val_acc'], label='128 units, 2 dropouts')
+plotr.title('Sarcasm detection learning curve')
+plotr.xlabel("Training epochs")
+plotr.ylabel("Network Accuracy")
+plotr.legend()
+plotr.show()
+plotr.clf()
+
+# Sequence vector model: Using convoluted neural network.
